5d20f4cb75da75aa3a145746f5fe2bc4071e7d14
Abel Babini Filho <abelbfilho@hotmail.com>
2020-06-28 08:43:55 -0300

    Backend Finalizado

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -4,6 +4,3 @@ export default {
     expiresIn: '1d',
   },
 };
-// MD5ONLINE => SecureGoBarberAuthenticatePhrase <==> d16d417f26b0570b9c4117061b3e29d0
-// refreshToken => Aplicação Mobile
-// JWT.io
